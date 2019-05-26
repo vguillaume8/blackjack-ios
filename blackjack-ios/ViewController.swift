070ed3636e2c15437b752356746d8fecbd1526cd
Vinston Guillaume <vinstonguillaume@gmail.com>
2019-05-26 00:13:03 -0400

    Initial Commit

---
+++ blackjack-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  blackjack-ios
+//
+//  Created by Vince G on 5/26/19.
+//  Copyright © 2019 Guillaume Corporations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
