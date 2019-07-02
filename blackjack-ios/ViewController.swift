1dac6d35b09423dff547f83bffae0283e5bfea28
Vinston Guillaume <vinstonguillaume@gmail.com>
2019-07-02 09:28:50 -0400

    added game score counter

--- blackjack-ios/ViewController.swift
+++ blackjack-ios/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {  
     @IBOutlet weak var leftImageView: UIImageView!
     @IBOutlet weak var rightImageView: UIImageView!
     @IBOutlet weak var leftScoreLabel: UILabel!
     @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    let defaults = UserDefaults.standard
+    
+    
+    @IBOutlet weak var playerGameScore: UILabel!
+    var playerGameScoreValue = 0
+    
+    @IBOutlet weak var cpuGameScore: UILabel!
+    var cpuGameScoreValue = 0
     
     var leftScore = 0
     var rightScore = 0
@@ -30,6 +40,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let getplayerGameScoreValue = defaults.value(forKey: "playerGameScore") ?? 0
+        let getcpuGameScoreValue = defaults.value(forKey: "cpuGameScore") ?? 0
+        
+        playerGameScoreValue = getplayerGameScoreValue as! Int
+        cpuGameScoreValue = getcpuGameScoreValue as! Int
+        
+        playerGameScore.text = String(playerGameScoreValue)
+        cpuGameScore.text = String(cpuGameScoreValue)
     }
 
     @IBAction func dealTapped(_ sender: Any) {
@@ -46,7 +64,7 @@ class ViewController: UIViewController {
     @IBAction func userStop(_ sender: Any) {
         // checks if the computer can still play
         if rightScore < computerLimit {
-            while rightScore < computerLimit && rightScore < leftScore {
+            while rightScore < computerLimit && rightScore <= leftScore {
                 drawCard(player: false)
             }
         }
@@ -56,18 +74,32 @@ class ViewController: UIViewController {
         }
     
         else if leftScore > rightScore {
-            
             gameOver(player: false)
         } else {
             
             if leftScore == rightScore {
                 drawGame()
             } else {
-                 gameOver(player: true)
+                gameOver(player: true)
             }
-           
         }
     }
+    
+    
+    @IBAction func startOver(_ sender: Any) {
+        let alert = UIAlertController(title: "Do you want to reset the scores?", message: "", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "No", style: .default, handler: { action in
+  
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+        self.reset()
+        }))
+       
+        self.present(alert, animated: true)
+    }
+    
     
     func convertCard(cardNumber: Int, player: Bool) -> Int {
 
@@ -86,12 +118,10 @@ class ViewController: UIViewController {
                     return 11
                 }
             }
-
          
-        }else if cardNumber > 10 {
+        } else if cardNumber > 10 {
             return 10
             
-
         } else if cardNumber > 10 {
             return 10
         }
@@ -139,9 +169,15 @@ class ViewController: UIViewController {
         if player == true {
             alert.title = "Sorry, You Lost! \(leftScore) - \(rightScore)"
             alert.message = "Better Luck Next Time!"
+            self.cpuGameScoreValue = self.cpuGameScoreValue + 1
+            self.cpuGameScore.text = String(self.cpuGameScoreValue)
+            defaults.set(self.cpuGameScoreValue, forKey: "cpuGameScore")
         } else {
             alert.title = "Congrats You Won! \(leftScore) - \(rightScore)"
-            alert.message = "Nice Job!" 
+            alert.message = "Nice Job!"
+            self.playerGameScoreValue = self.playerGameScoreValue + 1
+            self.playerGameScore.text = String(self.playerGameScoreValue)
+            defaults.set(self.playerGameScoreValue, forKey: "playerGameScore")
         }
         
         alert.addAction(UIAlertAction(title: "Play Again?", style: .default, handler: { action in
@@ -184,5 +220,15 @@ class ViewController: UIViewController {
         rightScore = 0
         leftScoreLabel.text = "0"
         rightScoreLabel.text = "0"
+    }
+    
+    func reset() {
+        restart()
+        playerGameScoreValue = 0
+        playerGameScore.text = "0"
+        cpuGameScoreValue = 0
+        cpuGameScore.text = "0"
+        defaults.set(0, forKey: "cpuGameScore")
+        defaults.set(0, forKey: "playerGameScore")
     }
 }
